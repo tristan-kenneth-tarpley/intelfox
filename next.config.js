fcf009069c5d1ae221e80e189a855dcd12862c47
Tristan Tarpley <me@tristantarpley.com>
2023-10-22 16:28:43 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailwindui.com',
+        port: '',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
