1c61f7ebde85f7b7b2f47abc950d91dcc80d9969
Tristan Tarpley <me@tristantarpley.com>
2023-11-10 15:56:27 -0500

    add exponential backoff to scraping bee requests

--- src/lib/services/scrapingBee/runScrapingBeeRequest.ts
+++ src/lib/services/scrapingBee/runScrapingBeeRequest.ts
@@ -1,3 +1,4 @@
+import { backOff } from 'exponential-backoff';
 import { AxiosResponse } from 'axios';
 import { ScrapingBeeClient } from 'scrapingbee';
 import scrapingBee from './scrapingBeeClient';
@@ -5,15 +6,19 @@ import scrapingBee from './scrapingBeeClient';
 const runScrapingBeeRequest = async (
   getterFn: (client: ScrapingBeeClient) => Promise<AxiosResponse<any, any>>,
 ) => {
-  const res = await getterFn(scrapingBee).catch((err) => {
-    // todo handle error
-    console.log('ScraperBee error', err.message);
-  });
+  try {
+    const res = await backOff(() => getterFn(scrapingBee), {
+      numOfAttempts: 3,
+    });
 
-  const decoder = new TextDecoder();
-  const text = res ? decoder.decode(res.data) : null;
+    const decoder = new TextDecoder();
+    const text = res ? decoder.decode(res.data) : null;
 
-  return text;
+    return text;
+  } catch (err) {
+    console.log('error running scraping bee request', err);
+    return null;
+  }
 };
 
 export default runScrapingBeeRequest;
